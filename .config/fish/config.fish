5af9c26d509e88521ed0925f4f58b1a588bb419e
Axel von Bertoldi <axel.bertoldi@emc.com>
2017-11-08 14:20:13 -0700

    Update fish config
    Requires omf be installed.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,12 @@
-# Path to your oh-my-fish.
-#set fish_path $HOME/.oh-my-fish
-
-#set SPGEAR_TOOLS_EXTRA /spgear/tools/bin /spgear/spgear/bin
-#set RUNNER_PATH /home/runner/bin
-#set PATH $HOME/.bin $PATH $RUNNER_PATH $SPGEAR_TOOLS_EXTRA
-
 # Theme
 set fish_theme bobthefish
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_color_branch magenta bold
+set theme_color_scheme gruvbox
+
+# Plugins
+#set fish_plugins vi-mode
+#set fish_plugins autojump vi-mode
+#set fish_plugins vi-mode fzf-autojump fzf
+
 
 # Aliases
 alias ls='ls --color=auto --group-directories-first'
@@ -22,14 +20,25 @@ alias gg="git grep"
 alias vimrc='gvim $HOME/.vimrc'
 alias ffind='find . -name'
 alias dof='git --git-dir=$HOME/.dotfiles.git'
+alias ll="exa -bgl --color=automatic --group-directories-first"
+alias la="exa -abgl --color=automatic --group-directories-first"
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
-# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
-# Example format: set fish_plugins autojump bundler
-set fish_plugins archlinux/pacman jump
+function qvim
+    nvim-qt $argv 2> /dev/null &
+end
 
-# Path to your custom folder (default path is $FISH/custom)
-#set fish_custom $HOME/dotfiles/oh-my-fish
+if test -e /usr/bin/yaourt
+  alias query='yaourt -Ss'
+  alias install='yaourt -S'
+  alias update='yaourt -Syyu'
+  alias updatea='yaourt -Syua --noconfirm'
+  alias uninstall='yaourt -Rsc'
+  alias orphaned='yaourt -Qdt'
+  alias not_installed='_search_not_installed'
+end
 
-# Load oh-my-fish configuration.
-#source $fish_path/oh-my-fish.fish
+# FZF
+#export FZF_CTRL_T_OPTS="--preview 'highlight -O ansi -l {} 2> /dev/null; or cat {} 2> /dev/null; or tree -C {} 2> /dev/null | head -200'"
+export FZF_CTRL_T_OPTS="--preview 'highlight -O ansi -l {} 2> /dev/null; or cat {} | head -200'"
+export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
